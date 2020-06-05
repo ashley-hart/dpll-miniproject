4b633647c522dc39127d1157d1813c1be42a9aa3
Ashley Hart <ashleyhart5618@gmail.com>
2020-06-05 19:14:21 -0400

    testing scripting

--- code/bench_runner.py
+++ code/bench_runner.py
@@ -2,9 +2,12 @@
 # UVA Summer Research Project
 # DPLL-SAT Solver Miniproject
 
+import os 
+import sys
 
 def main():
-    print("This code will used to run ")
+    print("This code will used to run benchmarks.")
+    os.system("cat knuth-sat.cnf")
 
 
 if __name__ == "__main__":
