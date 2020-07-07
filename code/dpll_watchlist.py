c975cb6b91214e10d161383aa5a69173415cb581
Ashley Hart <ashleyhart5618@gmail.com>
2020-07-07 13:44:22 -0400

    watchlist and recursion only

--- code/dpll_watchlist.py
+++ code/dpll_watchlist.py
@@ -8,6 +8,7 @@ This is a cleaner implementation of DPLL that uses a watchlist to update values.
 
 import copy
 
+# Setup watchlist
 def init_watchlist(literals, clauses, verbose):
 
     if verbose:
@@ -21,19 +22,12 @@ def init_watchlist(literals, clauses, verbose):
         wl.append([])
 
     for i in range(len(clauses)):
-
-        if verbose:
-            print("literals.index(clauses[",i,"][0]):", literals.index(clauses[i][0]))
-
         wl[literals.index(clauses[i][0])].append(clauses[i])
 
     return wl
 
-
+# Returns False if it is impossible to update watclist under an assignment.
 def update_watchlist(watchlist, literals, variables, assignment, false_literal, curr_var, clauses, verbose):
-
-    new_wl = copy.deepcopy(watchlist)
-    changed = False
 
     # Find index of sublist we need to access.
     fl_index = literals.index(false_literal)
@@ -46,107 +40,41 @@ def update_watchlist(watchlist, literals, variables, assignment, false_literal, 
         print("false literal index:", fl_index)
         print("assignment:", assignment)
         print("curr_var:", curr_var)
-        print("assignment[",curr_var,"] =",assignment[curr_var])
 
-    # Continue while we still have clauses looking at false_literal
-    while new_wl[fl_index]:
+    # Relocate all clauses watching false_literal.
+    while watchlist[fl_index]:
 
-        print("WORKING ROW = ", new_wl[fl_index])
-
-        # For every clause in the sublist, try to "move" it somewhere else.
-        clause = new_wl[fl_index][0]
+        clause = watchlist[fl_index][0]
+        changed = False
 
         for lit in clause:
-
-            # If we see a variable assigned to None or the negation of our 
-            # false literal then we move the clause and record the change.
-            # Should gloss over first index bc it shouldn't prompt an update.
             if assignment[abs(lit) - 1] == None or lit == false_literal * -1:
 
-                del new_wl[fl_index][0]
-                new_wl[literals.index(lit)].append(clause)
-                changed = True
-
                 if verbose:
+                    print("Deleted: ", watchlist[fl_index][0])
                     print("Moved", clause, "to watchlist[",(literals.index(lit)),"]...")
-                    print("NEW ROW: ", new_wl[literals.index(lit)])
-                    # print("curr watchlist:", new_wl)
+                    print("Current watchlist:", watchlist)
 
+                del watchlist[fl_index][0]
+                watchlist[literals.index(lit)].append(clause)
+                changed = True  
                 break
 
-        # If we cannot do this for any clause then we return False
+        # If we cannot eliminate a clause that is watching false_literal, return False
         if not changed: 
             if verbose:
-                print("UPDATE_WL(): Was unable to update watchlist under given assignment.")
+                print("ERROR - UPDATE_WL(): Was unable to update watchlist under given assignment.")
                 print("watchlist:", watchlist)
                 print("literals:", literals)
+                print("variables:", variables)
                 print("false literal:", false_literal)
                 print("assignment:", assignment)
-            return ([], False)
+                print()
+            return False
 
-    return (new_wl, changed)
+    return True
 
-
-def solve(problem):
-
-    literals = []
-    variables = []
-    assignment = []
-
-    for c in problem.clauses:
-        for lit in c:
-
-            if abs(lit) not in variables:
-                variables.append(abs(lit))
-                assignment.append(None)
-
-                if lit < 0:
-                    literals.append(abs(lit))
-                    literals.append(lit)
-                else:
-                    literals.append(abs(lit))
-                    literals.append(lit * -1)
-
-    literals.sort(key=abs)
-
-    watchlist = init_watchlist(literals, problem.clauses, problem.verbose)
-
-    if problem.verbose:
-        print("variables:", variables)
-        print("literals:", literals)
-        print("watchlist:", watchlist)
-        print("assignment:", assignment)
-
-    result = dpll(watchlist, problem.clauses, assignment, literals, variables, 0, problem.verbose)
-
-    if problem.verbose:
-        print("Final result:", result)
-
-    return result
-
-
-def SAT_check(clauses, assignment, verbose):
-    t_vals = []
-    temp = []
-
-    for c in clauses:
-        for lit in c:
-            if assignment[abs(lit) - 1] == True:
-                if lit > 0:
-                    temp.append(True)
-                if lit < 0:
-                    temp.append(False)
-            else: 
-                if lit > 0:
-                    temp.append(False)
-                if lit < 0:
-                    temp.append(True)
-        t_vals.append(temp)
-        temp = []
-    
-    return (assignment, clause_check(t_vals, verbose))
-
-
+# Checks if truth values are SAT or UNSAT.
 def clause_check(t_vals, verbose):
     is_SAT: bool = True
 
@@ -166,52 +94,111 @@ def clause_check(t_vals, verbose):
 
     return is_SAT
 
+# Sets up the truth values and passes them to clause check.
+def SAT_check(clauses, assignment, verbose):
+    t_vals = []
+    temp = []
+
+    for c in clauses:
+        for lit in c:
+            if assignment[abs(lit) - 1] == True:
+                if lit > 0:
+                    temp.append(True)
+                if lit < 0:
+                    temp.append(False)
+            elif assignment[abs(lit) - 1] == False: 
+                if lit > 0:
+                    temp.append(False)
+                if lit < 0:
+                    temp.append(True)
+            else:
+                temp.append(None)
+
+        t_vals.append(temp)
+        temp = []
+    
+    return clause_check(t_vals, verbose)
+
+
+def solve(problem):
+
+    literals = []
+    variables = []
+    assignment = []
+
+    for c in problem.clauses:
+        for lit in c:
+            if abs(lit) not in variables:
+                variables.append(abs(lit))
+                assignment.append(None)
+
+                if lit < 0:
+                    literals.append(abs(lit))
+                    literals.append(lit)
+                else:
+                    literals.append(abs(lit))
+                    literals.append(lit * -1)
+
+    literals.sort(key=abs)
+    variables.sort()
+
+    watchlist = init_watchlist(literals, problem.clauses, problem.verbose)
+
+    if problem.verbose:
+        print("variables:", variables)
+        print("literals:", literals)
+        print("watchlist:", watchlist)
+        print("assignment:", assignment)
+
+    result = dpll(watchlist, problem.clauses, assignment, literals, variables, 0, problem.verbose)
+
+    if problem.verbose:
+        print("Final result:", result)
+        print("result:", result)
+
+    return result
+
 
 def dpll(watchlist, clauses, assignment, literals, variables, curr_var,  verbose):
 
-    # TODO: Reimplement unit propagation
-    # TODO: Reimplement pure literal elimination
-    # TODO: Adjust the recursive engine to update the watchlist after every assignment.
+    false_literal = 0
 
-    false_literal: int = 0
-
-    if curr_var >= len(variables):
-        if verbose:
-            print("Base Case")
-
-        return (assignment, SAT_check(clauses, assignment, verbose))
-
+    # NOTE: Might be uneccessary.
+    if curr_var == (len(variables)):
+        return SAT_check(clauses,assignment, verbose)
 
     for a in [True, False]:
-
         assignment[curr_var] = a
 
         if a == True:
             false_literal = variables[curr_var] * -1
         else:
-            false_literal = variables[curr_var]
+            false_literal = abs(variables[curr_var])
 
-        data = update_watchlist(watchlist, literals, variables, assignment, false_literal, curr_var, clauses, verbose)
-        updated_wl = data[1]
 
         result = SAT_check(clauses, assignment, verbose)
 
         if verbose:
-            print("assignment:", assignment)
             print("false_literal:", false_literal)
-            print("SAT_check results:", result)
-            
-        if result[1] == True:
-            return (assignment, True)
-        elif updated_wl:
-            data = dpll(watchlist, clauses, assignment, literals, variables, curr_var + 1, verbose)
+            print("assignment:", assignment)
+            print("curr_var:", curr_var)
 
-            if data[1] == True:
-                return (assignment, True)
+        # If we find a satisfiyng argument then we don't need to go any further
+        if result == True:
+            return True
+        # If it is unknown if our assignment is SAT or UNSAT then we check if we can 
+        # update our watchlist and try to make another call.
+        elif result == None:
+            # If we can update our watchlist then we still have a working assignment
+            if update_watchlist(watchlist, literals, variables, assignment, false_literal, curr_var, clauses, verbose):
+                if dpll(watchlist, clauses, assignment, literals, variables, curr_var + 1, verbose) is True:
+                    return True
+                else: 
+                    assignment[curr_var] = None
+        # If we know our assignment is UNSAT, try the other option before failing out.
+        else:
+            assignment[curr_var] = None
 
-
-    assignment[curr_var] = None
-
-    return ([], False)
-
+    return False
+                
 
