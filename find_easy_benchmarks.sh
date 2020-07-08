5bb228e2f1077016ca69a347c40f8e9e81680509
Ashley Hart <ashleyhart5618@gmail.com>
2020-07-08 12:14:11 -0400

    updated script

--- find_easy_benchmarks.sh
+++ find_easy_benchmarks.sh
@@ -10,10 +10,11 @@ SAT_SOLVER=code/solver.py
 
 # Loop through all the files in the current directory
 # with a .cnf extension; give the current file the name "f"
-for f in my-cnf/*.cnf; do
+for f in benchmarks/sr2019/*/*.cnf; do
     # Stop the solver if it's still running
     # after 10 seconds
-    timeout 10 python3 $SAT_SOLVER $f -r
+    echo $f
+    timeout 10 python3 $SAT_SOLVER $f -l
     
     # The exit status is stored in a variable
     # named "?"...I don't know why
@@ -23,7 +24,25 @@ for f in my-cnf/*.cnf; do
     if [[ $exit_status -eq 0 ]]; then
 	# Append the name of the successful file
 	# to the end of the "easy list"
-	echo $f >> easy_benchmarks.txt
+	echo $f >> easy_benchmarks_le.txt
+    fi
+done 
+
+for f in benchmarks/my-cnf/*.cnf; do
+    # Stop the solver if it's still running
+    # after 10 seconds
+    echo $f
+    timeout 10 python3 $SAT_SOLVER $f -l
+    
+    # The exit status is stored in a variable
+    # named "?"...I don't know why
+    exit_status=$?
+    
+    # An exit status of "0" means success
+    if [[ $exit_status -eq 0 ]]; then
+	# Append the name of the successful file
+	# to the end of the "easy list"
+	echo $f >> easy_benchmarks_le.txt
     fi
 done
 
